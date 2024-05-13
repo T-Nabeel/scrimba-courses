531148d61c817c39ba58d37d2d727653cd6d0dcb
T-Nabeel <nmtn3030@gmail.com>
2024-05-13 09:10:49 +0530

    Refactor to template string

--- learn-javascript/3.chrome-extension/script.js
+++ learn-javascript/3.chrome-extension/script.js
@@ -7,30 +7,14 @@ saveBtn.addEventListener("click", saveLead);
 
 function saveLead() {
   myLeads.push(inputEl.value);
-  inputEl.value = ""
-  renderLeads()
+  inputEl.value = "";
+  renderLeads();
 }
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + "<a href= " + myLeads[i] + " target='_blank'>" + myLeads[i] + "</a>" +"</li>";
+    listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
   }
   ulEl.innerHTML = listItems;
 }
-
-// Practice
-// const box = document.getElementById("box")
-
-// box.addEventListener("click", alertMsg)
-// function alertMsg() {
-//     alert("You have opened the box, here's a cookie :)")
-// }
-
-// const cont = document.getElementsByClassName("container")
-// const cont = document.getElementById("container")
-// cont.innerHTML = "<button onclick='buy()'>Buy!</button>"
-
-// function buy() {
-//     cont.innerHTML += "<p>Thank you for buying!</p>"
-// }
